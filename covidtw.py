9a0453062cbdaf8142f2e20680beb1476286dc6f
jasonleetw <70790151+jasonleetw@users.noreply.github.com>
2021-06-02 15:42:25 +0800

    202106021542

--- covidtw.py
+++ covidtw.py
@@ -5,21 +5,40 @@ from urllib import request
 import json
 import ssl
 
-fp = open("result.js", "w", encoding='utf-8')
-fp.write('var data_3 = [')
 
-url = 'https://covid-19.nchc.org.tw/myDT_staff.php?TB_name=csse_covid_19_daily_reports_country&limitColumn=a01&limitValue=TW/%&equalValue=+like+&encodeKey=MTYyMjUwNDY5NQ==&c[]=id&t[]=int&d[]=NO&c[]=a01&t[]=varchar&d[]=NO&c[]=a02&t[]=date&d[]=NO&c[]=a03&t[]=int&d[]=NO&c[]=a04&t[]=int&d[]=NO&c[]=a05&t[]=int&d[]=NO&c[]=a06&t[]=int&d[]=NO&c[]=a07&t[]=decimal&d[]=NO&c[]=a08&t[]=decimal&d[]=NO&c[]=a09&t[]=decimal&d[]=NO&c[]=a10&t[]=decimal&d[]=NO&c[]=a11&t[]=decimal&d[]=NO'
+fp = open("daily_reports_vaccine_city.js", "w", encoding='utf-8')
+fp.write('var data_4 = [')
+
+url = 'https://covid-19.nchc.org.tw/myDT_staff.php?TB_name=csse_covid_19_daily_reports_vaccine_city_c&limitColumn=id&limitValue=0&equalValue=!=&encodeKey=MTYyMjU3OTYwNA==&c[]=id&t[]=int&d[]=NO&c[]=a01&t[]=date&d[]=NO&c[]=a02&t[]=varchar&d[]=NO&c[]=a03&t[]=int&d[]=NO&c[]=a04&t[]=int&d[]=YES&c[]=a05&t[]=int&d[]=YES&c[]=a06&t[]=int&d[]=YES&c[]=a07&t[]=decimal&d[]=YES&c[]=a08&t[]=varchar&d[]=NO&c[]=a09&t[]=decimal&d[]=NO'
 
 context = ssl._create_unverified_context()
 with request.urlopen(url, context=context) as jsondata:
     data = json.loads(jsondata.read().decode())
-rows = sorted(data['data'], key=lambda d: d['a02'])
+rows = sorted(data['data'], key=lambda d: d['a01'], reverse=True)
 for row in rows:
-    x = datetime.strptime(row['a02'], '%Y-%m-%d')
-    if x < datetime.strptime("2021-05-14", "%Y-%m-%d"):
-        continue
-    fp.write("{{year: '{}',Confirmed:{}, Deaths:{}, Recovered:{}, Active: {}, }},".format(
-        datetime.strftime(x, "%m/%d"), row['a03'], row['a04'], row['a05'], row['a06']))
+    # x = datetime.strptime(row['a02'], '%Y-%m-%d')
+    # if x < datetime.strptime("2021-05-14", "%Y-%m-%d"):
+    #     continue
+    # fp.write("{{year: '{}',Confirmed:{}, Deaths:{}, Recovered:{}, Active: {}, }},".format(
+    #     datetime.strftime(x, "%m/%d"), row['a03'], row['a04'], row['a05'], row['a06']))
+    fp.write("{{1: '{}',2:{}, 3:{}, 4:{}, 5: {}, 6: {}, 7: {}, 8: {}, 9: {},}},\n".format(
+        row['a01'], row['a02'], row['a03'], row['a04'], row['a05'], row['a06'], row['a07'], row['a08'], row['a09']))
+
+# fp = open("result.js", "w", encoding='utf-8')
+# fp.write('var data_3 = [')
+
+# url = 'https://covid-19.nchc.org.tw/myDT_staff.php?TB_name=csse_covid_19_daily_reports_country&limitColumn=a01&limitValue=TW/%&equalValue=+like+&encodeKey=MTYyMjUwNDY5NQ==&c[]=id&t[]=int&d[]=NO&c[]=a01&t[]=varchar&d[]=NO&c[]=a02&t[]=date&d[]=NO&c[]=a03&t[]=int&d[]=NO&c[]=a04&t[]=int&d[]=NO&c[]=a05&t[]=int&d[]=NO&c[]=a06&t[]=int&d[]=NO&c[]=a07&t[]=decimal&d[]=NO&c[]=a08&t[]=decimal&d[]=NO&c[]=a09&t[]=decimal&d[]=NO&c[]=a10&t[]=decimal&d[]=NO&c[]=a11&t[]=decimal&d[]=NO'
+
+# context = ssl._create_unverified_context()
+# with request.urlopen(url, context=context) as jsondata:
+#     data = json.loads(jsondata.read().decode())
+# rows = sorted(data['data'], key=lambda d: d['a02'])
+# for row in rows:
+#     x = datetime.strptime(row['a02'], '%Y-%m-%d')
+#     if x < datetime.strptime("2021-05-14", "%Y-%m-%d"):
+#         continue
+#     fp.write("{{year: '{}',Confirmed:{}, Deaths:{}, Recovered:{}, Active: {}, }},".format(
+#         datetime.strftime(x, "%m/%d"), row['a03'], row['a04'], row['a05'], row['a06']))
 
 fp.write('];\n')
 fp.close()
